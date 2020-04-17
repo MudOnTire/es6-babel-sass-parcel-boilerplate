24caeaa2ba8b0455bc3fc249804e127fbcc3e15d
13851731474 <13851731474@localhost>
2020-04-17 11:28:44 +0800

    finish

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
-console.log('it works');
+import Header from './components/header';
+import User from './components/user';
+
+import './scss/app.scss';
+
+const app = async ()=>{
+  document.getElementById('header').innerHTML = Header();
+  document.getElementById('user').innerHTML = await User();
+}
+
+app();
---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+const Header = ()=>{
+  const template = `
+    <header>
+      <h1>My Parcel App</h1>
+      <p>This is a boilerplate for a simple JS workflow using parcel</p> 
+    </header>
+  `;
+
+  return template;
+}
+
+export default Header;
